da08879765fb7d111338ffe6bc09189af1faa1b6
Dung Nguyen <dungvippro1711@gmail.com>
2026-09-13 15:28:08 +0700

    feat(dbt): add customer dimension

---
+++ dbt/glamira_analytics/models/marts/dimensions/dim_customer.sql
@@ -0,0 +1,50 @@
+{{ config(
+    materialized='table'
+) }}
+
+WITH known_customers AS (
+
+    SELECT DISTINCT
+        customer_id
+
+    FROM {{ ref('int_checkout_deduplicated') }}
+
+    WHERE customer_id IS NOT NULL
+
+)
+
+,unknown_customer AS (
+
+    SELECT
+        CAST(0 AS INT64) AS customer_key
+        ,CAST(NULL AS STRING) AS customer_id
+        ,'UNKNOWN' AS customer_type
+        ,FALSE AS is_registered
+
+)
+
+,registered_customers AS (
+
+    SELECT
+        FARM_FINGERPRINT(
+            CONCAT(
+                'customer|',
+                customer_id
+            )
+        ) AS customer_key
+
+        ,customer_id
+        ,'REGISTERED' AS customer_type
+        ,TRUE AS is_registered
+
+    FROM known_customers
+
+)
+
+SELECT *
+FROM unknown_customer
+
+UNION ALL
+
+SELECT *
+FROM registered_customers
